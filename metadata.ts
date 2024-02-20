72f890d762218b726b5e2bdcf77a4a453f11a4ab
Andy <deltandy123@proton.me>
2024-02-20 18:39:32 -0500

    Fix invalid literal

--- metadata.ts
+++ metadata.ts
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 
 const infoPlist = Plist.parse(readFileSync("discord/Payload/Discord.app/Info.plist", "utf8")) as {[key: string]: any};
 
-console.log({
+console.log(JSON.stringify({
   name: infoPlist.CFBundleName,
   version: infoPlist.CFBundleVersion
-});
+}));
